97157653c1e3d50d3143c800a49ca0f3e202d261
Michael <MichaelStruck@users.noreply.github.com>
2015-03-08 14:19:57 -0700

    Update Switch Changes Mode.groovy
    updated version and date within code

--- IFTTT-SmartApps/Switch-Changes-Mode/Switch Changes Mode.groovy
+++ IFTTT-SmartApps/Switch-Changes-Mode/Switch Changes Mode.groovy
@@ -2,7 +2,7 @@
  *  Switch Changes Mode
  *
  *  Copyright 2015 Michael Struck
- *  Version 1.0 3/7/15
+ *  Version 1.01 3/8/15
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
