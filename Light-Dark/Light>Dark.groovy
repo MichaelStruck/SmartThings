232f488deb9a133b9d18827bc5e023fd148d9cef
Michael <MichaelStruck@users.noreply.github.com>
2015-03-12 15:49:26 -0700

    Update Light>Dark.groovy

--- Light-Dark/Light>Dark.groovy
+++ Light-Dark/Light>Dark.groovy
@@ -2,13 +2,13 @@
  *  Light > Dark
  *  Version 1.0 3/6/15
  *
- *	Using code from SmartThings Light Up The Night App and the Sunrise/Sunset app
+ *  Using code from SmartThings Light Up The Night App and the Sunrise/Sunset app
  *  Copyright 2015 Michael Struck
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
