d19faaf84f2ee37de7c1320c6ca4e23444b7ca78
Michael <MichaelStruck@users.noreply.github.com>
2015-03-26 11:42:10 -0700

    Update Nest Helper-Mode Change.groovy

--- Other-SmartApps/Nest-Helper-Mode-Change/Nest Helper-Mode Change.groovy
+++ Other-SmartApps/Nest-Helper-Mode-Change/Nest Helper-Mode Change.groovy
@@ -1,7 +1,7 @@
 /**
  *  Nest Helper-Mode Change
  *
- *	Version 1.0 3/26/15
+ *  Version 1.0 3/26/15
  *  Copyright 2015 Michael Struck
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
