c992b03bf064a3d01900847d60f8123fb0810ba8
Michael <MichaelStruck@users.noreply.github.com>
2015-03-28 18:18:52 -0700

    Update SmartHomeVentilation.groovy

--- Other-SmartApps/SmartHomeVentilation/SmartHomeVentilation.groovy
+++ Other-SmartApps/SmartHomeVentilation/SmartHomeVentilation.groovy
@@ -1,6 +1,6 @@
 /**
  *  Smart Home Ventilation 3/27/15
- *  Version 1.00
+ *  Version 1.00 3/28/15
  * 
  *
  *  Copyright 2015 Michael Struck
@@ -20,6 +20,7 @@ definition(
     namespace: "MichaelStruck",
     author: "Michael Struck",
     description: "Allows for setting up schedules for turning on and off home ventilation fan.",
+    category: "Convenience",
     iconUrl: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/SmartHomeVentilation/HomeVent.png",
     iconX2Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/SmartHomeVentilation/HomeVent@2x.png",
     iconX3Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/SmartHomeVentilation/HomeVent@2x.png")
@@ -29,13 +30,21 @@ preferences {
     section("Select home ventilation switch..."){
 		input "switch1", title: "Switch", "capability.switch", multiple: false
 	}
-    section("Daytime Schedule..."){
-		input "timeOnDay", title: "Time to turn on", "time"
-        input "timeOffDay", title: "Time to turn off", "time"
+    section("Daytime Schedule 1..."){
+		input "timeOnDay1", title: "Time to turn on", "time"
+        input "timeOffDay1", title: "Time to turn off", "time"
 	}
-    section("Nighttime Schedule..."){
-        input "timeOnNight", title: "Time to turn on", "time"
-        input "timeOffNight", title: "Time to turn off", "time"
+    section("Daytime Schedule 2..."){
+		input "timeOnDay2", title: "Time to turn on", "time", required: false
+        input "timeOffDay2", title: "Time to turn off", "time", required: false
+	}
+    section("Nighttime Schedule 1..."){
+        input "timeOnNight1", title: "Time to turn on", "time", required: false
+        input "timeOffNight1", title: "Time to turn off", "time", required: false
+	}
+    section("Nighttime Schedule 2..."){
+        input "timeOnNight2", title: "Time to turn on", "time", required: false
+        input "timeOffNight2", title: "Time to turn off", "time", required: false
 	}
 }
 
@@ -51,25 +60,52 @@ def updated() {
 }
 
 def init () {
-    schedule(timeOnDay, "turnOnDay")
-    schedule(timeOffDay, "turnOffDay")
-    schedule(timeOnNight, "turnOnNight")	
-    schedule(timeOffNight, "turnOffNight")
+    schedule(timeOnDay1, "turnOnDay1")
+    schedule(timeOffDay1, "turnOffDay1")
+    
+    if (timeOnDay2) {
+        schedule(timeOnDay2, "turnOnDay2")
+    	schedule(timeOffDay2, "turnOffDay2")
+    }
+    if (timeOnNight1){
+    	schedule(timeOnNight1, "turnOnNight1")	
+    	schedule(timeOffNight1, "turnOffNight1")
+    }
+    if (timeOnNight2){
+    	schedule(timeOnNight2, "turnOnNight2")	
+    	schedule(timeOffNight2, "turnOffNight2")
+    }
 }
 
-def turnOffDay() {
+def turnOffDay1() {
     turnOffSwitch()
 }
 
-def turnOnDay() {
+def turnOffDay2() {
+    turnOffSwitch()
+}
+
+def turnOnDay1() {
     turnOnSwitch()
 }
 
-def turnOnNight() {
+def turnOnDay2() {
     turnOnSwitch()
 }
 
-def turnOffNight() {
+def turnOnNight1() {
+    turnOnSwitch()
+}
+
+def turnOnNight2() {
+    turnOnSwitch()
+}
+
+def turnOffNight1() {
+    turnOffSwitch()
+}
+
+def turnOffNight2() {
     turnOffSwitch()
 }
 
