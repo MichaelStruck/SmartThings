c40b7e1da31211256ab5ede579f90ab6d0cf6698
Michael <MichaelStruck@users.noreply.github.com>
2015-05-25 10:31:22 -0700

    Update Light-Dark.groovy

--- Other-SmartApps/Light-Dark/Light-Dark.groovy
+++ Other-SmartApps/Light-Dark/Light-Dark.groovy
@@ -1,12 +1,13 @@
 /**
  *  Light > Dark
- *  Version 1.05 5/17/15
+ *  Version 1.05 5/25/15
  *
- *	1.01 Added a verify so they event has to trip trice in a row to do the action.
- *	1.02 Added custom icon
+ *  1.01 Added a verify so they event has to trip trice in a row to do the action.
+ *  1.02 Added custom icon
  *  1.03 Revision to interface for better flow
- *	1.04 Added dimmer switches/levels, reorganized interface and added time restrictions options
+ *  1.04 Added dimmer switches/levels, reorganized interface and added time restrictions options
  *  1.05 Fixed an inconsistent time limitiation code issue
+ *  1.06 Added an About screen
  *
  *
  *	Using code from SmartThings Light Up The Night App and the Sunrise/Sunset app
@@ -35,7 +36,8 @@ definition(
     )
 
 preferences {
-	page(name: "getPref")
+	page name:"getPref"
+    page name:"pageAbout"
 }
 
 def getPref() {
@@ -61,10 +63,22 @@ def getPref() {
 			label(title: "Assign a name", required: false, defaultValue: "Light > Dark")
 		    href "timeIntervalInput", title: "Set for specific times", description: getTimeLabel(timeStart, timeEnd), state: greyedOutTime(timeStart, timeEnd), refreshAfterSelection:true
             mode title: "Set for specific mode(s)", required: false
+            href "pageAbout", title: "About ${textAppName()}", description: "Tap to get version and license information"
         }
 	}
 }
 
+def pageAbout() {
+	dynamicPage(name: "pageAbout", title: "About ${textAppName()}") {
+        section {
+            paragraph "${textVersion()}\n${textCopyright()}\n\n${textHelp()}\n"
+        }
+        section("License") {
+            paragraph textLicense()
+        }
+    }
+}
+//------------------------------------------------
 def installed() {
 	log.debug "Installed with settings: ${settings}"
     init()
@@ -164,3 +178,38 @@ page(name: "timeIntervalInput", title: "Run only during a certain time", refresh
 			input "timeEnd", "time", title: "Ending", required: false, refreshAfterSelection:true
 		}
         }
+
+//Version/Copyright/Information/Help
+
+private def textAppName() {
+	def text = "Light > Dark"
+}	
+
+private def textVersion() {
+    def text = "Version 1.0.6 (05/25/2015)"
+}
+
+private def textCopyright() {
+    def text = "Copyright © 2015 Michael Struck"
+}
+
+private def textLicense() {
+    def text =
+        "This program is free software: you can redistribute it and/or " +
+        "modify it under the terms of the GNU General Public License as " +
+        "published by the Free Software Foundation, either version 3 of " +
+        "the License, or (at your option) any later version.\n\n" +
+        "This program is distributed in the hope that it will be useful, " +
+        "but WITHOUT ANY WARRANTY; without even the implied warranty of " +
+        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU " +
+        "General Public License for more details.\n\n" +
+        "You should have received a copy of the GNU General Public License " +
+        "along with this program. If not, see <http://www.gnu.org/licenses/>."
+}
+
+private def textHelp() {
+	def text =
+    	"Instructions:\nSimilar to a night light, you can choose what lights or dimmers go on and then, separately, " +
+        "which lights or dimmers go off based on the lux setting of a light sensor. In addition, when the lux thresholds are " +
+        "met, different modes can be set." 
+}
