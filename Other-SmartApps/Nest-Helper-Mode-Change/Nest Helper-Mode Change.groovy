47cb9716b93e79c6aaa72e78fd912518d445e8dd
Michael <MichaelStruck@users.noreply.github.com>
2015-05-25 10:41:39 -0700

    Update Nest Helper-Mode Change.groovy

--- Other-SmartApps/Nest-Helper-Mode-Change/Nest Helper-Mode Change.groovy
+++ Other-SmartApps/Nest-Helper-Mode-Change/Nest Helper-Mode Change.groovy
@@ -2,8 +2,9 @@
  *  Nest Helper-Mode Change
  *
  *  Copyright 2015 Michael Struck
- *  Version 1.00 3/26/15
+ *	Version 1.00 3/26/15
  *  Version 1.01 - 4/9/15 Added the ability to change the name of the app and removed modes to run in (redundent)
+ *  Version 1.0.2 - 5/25/15 Added About screen
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -26,7 +27,8 @@ definition(
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Partner/nest-home-app@2x.png")
 
 preferences {
-	page(name: "getPref")
+	page name:"getPref"
+    page name:"pageAbout"
 }
 
 def getPref() {
@@ -43,9 +45,23 @@ def getPref() {
 		}
 		section([mobileOnly:true], "Options") {
 			label(title: "Assign a name", required: false, defaultValue: "Nest Helper-Mode Change")
+            href "pageAbout", title: "About ${textAppName()}", description: "Tap to get version and license information"
 		}
     }
 }
+
+def pageAbout() {
+	dynamicPage(name: "pageAbout", title: "About ${textAppName()}") {
+        section {
+            paragraph "${textVersion()}\n${textCopyright()}\n\n${textHelp()}\n"
+        }
+        section("License") {
+            paragraph textLicense()
+        }
+    }
+}
+
+//--------------------------------------
 
 def installed() {
 	log.debug "Installed with settings: ${settings}"
@@ -70,4 +86,38 @@ def locationHandler(evt) {
     else if (awayMode.contains(evt.value)) {
     	tstat1.away()
     }
+}
+
+//Version/Copyright/Information/Help
+
+private def textAppName() {
+	def text = "Nest Helper-Change Modes"
+}	
+
+private def textVersion() {
+    def text = "Version 1.0.2 (05/25/2015)"
+}
+
+private def textCopyright() {
+    def text = "Copyright © 2015 Michael Struck"
+}
+
+private def textLicense() {
+    def text =
+        "This program is free software: you can redistribute it and/or " +
+        "modify it under the terms of the GNU General Public License as " +
+        "published by the Free Software Foundation, either version 3 of " +
+        "the License, or (at your option) any later version.\n\n" +
+        "This program is distributed in the hope that it will be useful, " +
+        "but WITHOUT ANY WARRANTY; without even the implied warranty of " +
+        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU " +
+        "General Public License for more details.\n\n" +
+        "You should have received a copy of the GNU General Public License " +
+        "along with this program. If not, see <http://www.gnu.org/licenses/>."
+}
+
+private def textHelp() {
+	def text =
+    	"Instructions:\nChoose a Nest Thermostat that will be linked to various SmartThings modes. " +
+        "When the modes change, the Nest Thermostat will change to Home or Away based on the settings." 
 }
