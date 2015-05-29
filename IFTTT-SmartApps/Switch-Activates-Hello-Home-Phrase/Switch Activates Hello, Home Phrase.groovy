5189a1b54e1db0d97a0d49cb99dfe73b14b2a44e
Michael <MichaelStruck@users.noreply.github.com>
2015-05-29 13:57:22 -0700

    Update Switch Activates Hello, Home Phrase.groovy

--- IFTTT-SmartApps/Switch-Activates-Hello-Home-Phrase/Switch Activates Hello, Home Phrase.groovy
+++ IFTTT-SmartApps/Switch-Activates-Hello-Home-Phrase/Switch Activates Hello, Home Phrase.groovy
@@ -2,8 +2,9 @@
  *  Switch Activates Hello, Home Phrase
  *
  *  Copyright 2015 Michael Struck
- *  Version 1.01 3/8/15
- *  Version 1.02 4/24/15 added the ability to rename the app and limit it to certain modes
+ *  Version 1.0.1 3/8/15
+ *  Version 1.0.2 4/24/15 added the ability to rename the app and limit it to certain modes
+ *  Version 1.0.3 5/29/15 Added an About Screen
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -53,8 +54,18 @@ def getPref() {
 		section([mobileOnly:true], "Options") {
 			label(title: "Assign a name", required: false)
             mode title: "Set for specific mode(s)", required: false
+            href "pageAbout", title: "About ${textAppName()}", description: "Tap to get version and license information"
 		}
     }
+}
+
+page(name: "pageAbout", title: "About ${textAppName()}") {
+	section {
+		paragraph "${textVersion()}\n${textCopyright()}\n\n${textHelp()}\n"
+	}
+	section("License") {
+		paragraph textLicense()
+	}
 }
 
 def installed() {
@@ -76,3 +87,38 @@ def switchHandler(evt) {
     }
 }
 
+//Version/Copyright/Information/Help
+
+private def textAppName() {
+	def text = "Switch Activates Hello, Home Phrase"
+}	
+
+private def textVersion() {
+    def text = "Version 1.0.3 (05/29/2015)"
+}
+
+private def textCopyright() {
+    def text = "Copyright © 2015 Michael Struck"
+}
+
+private def textLicense() {
+    def text =
+		"Licensed under the Apache License, Version 2.0 (the 'License'); "+
+		"you may not use this file except in compliance with the License. "+
+		"You may obtain a copy of the License at"+
+		"\n\n"+
+		"    http://www.apache.org/licenses/LICENSE-2.0"+
+		"\n\n"+
+		"Unless required by applicable law or agreed to in writing, software "+
+		"distributed under the License is distributed on an 'AS IS' BASIS, "+
+		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. "+
+		"See the License for the specific language governing permissions and "+
+		"limitations under the License."
+}
+
+private def textHelp() {
+	def text =
+    	"Instructions:\nTies a Hello, Home phrase to a switch (virtual or real) on/off state. Perfect for use with IFTTT. "+
+		"Simple define a switch to be used, then tie the on/off state of the switch to a specific Hello, Home phrases. "+
+		"Connect the switch to an IFTTT action, and the Hello, Home phrase will fire with the switch state change." 
+}
