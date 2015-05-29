353ecaf9217945f9c1547002ed74e810b607f400
Michael <MichaelStruck@users.noreply.github.com>
2015-05-29 13:37:17 -0700

    Update Light-Dark.groovy

--- Other-SmartApps/Light-Dark/Light-Dark.groovy
+++ Other-SmartApps/Light-Dark/Light-Dark.groovy
@@ -2,10 +2,10 @@
  *  Light > Dark
  *  Version 1.05 5/25/15
  *
- *  1.01 Added a verify so they event has to trip trice in a row to do the action.
- *  1.02 Added custom icon
+ *	1.01 Added a verify so they event has to trip trice in a row to do the action.
+ *	1.02 Added custom icon
  *  1.03 Revision to interface for better flow
- *  1.04 Added dimmer switches/levels, reorganized interface and added time restrictions options
+ *	1.04 Added dimmer switches/levels, reorganized interface and added time restrictions options
  *  1.05 Fixed an inconsistent time limitiation code issue
  *  1.06 Added an About screen
  *
@@ -195,16 +195,17 @@ private def textCopyright() {
 
 private def textLicense() {
     def text =
-        "This program is free software: you can redistribute it and/or " +
-        "modify it under the terms of the GNU General Public License as " +
-        "published by the Free Software Foundation, either version 3 of " +
-        "the License, or (at your option) any later version.\n\n" +
-        "This program is distributed in the hope that it will be useful, " +
-        "but WITHOUT ANY WARRANTY; without even the implied warranty of " +
-        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU " +
-        "General Public License for more details.\n\n" +
-        "You should have received a copy of the GNU General Public License " +
-        "along with this program. If not, see <http://www.gnu.org/licenses/>."
+		"Licensed under the Apache License, Version 2.0 (the 'License');"+
+		"you may not use this file except in compliance with the License."+
+		"You may obtain a copy of the License at"+
+		"\n\n"+
+		"    http://www.apache.org/licenses/LICENSE-2.0"+
+		"\n\n"+
+		"Unless required by applicable law or agreed to in writing, software"+
+		"distributed under the License is distributed on an 'AS IS' BASIS,"+
+		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."+
+		"See the License for the specific language governing permissions and"+
+		"limitations under the License."
 }
 
 private def textHelp() {
