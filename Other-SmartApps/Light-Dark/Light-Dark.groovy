7c69173f8a7de943389389c2aa5f61515349ee83
Michael <MichaelStruck@users.noreply.github.com>
2015-05-30 09:29:54 -0700

    Update Light-Dark.groovy

--- Other-SmartApps/Light-Dark/Light-Dark.groovy
+++ Other-SmartApps/Light-Dark/Light-Dark.groovy
@@ -1,11 +1,11 @@
 /**
  *  Light > Dark
- *  Version 1.05 5/25/15
+ *  Version 1.0.6 5/25/15
  *
- *	1.01 Added a verify so they event has to trip trice in a row to do the action.
- *	1.02 Added custom icon
+ *  1.01 Added a verify so they event has to trip trice in a row to do the action.
+ *  1.02 Added custom icon
  *  1.03 Revision to interface for better flow
- *	1.04 Added dimmer switches/levels, reorganized interface and added time restrictions options
+ *  1.04 Added dimmer switches/levels, reorganized interface and added time restrictions options
  *  1.05 Fixed an inconsistent time limitiation code issue
  *  1.06 Added an About screen
  *
@@ -177,7 +177,7 @@ page(name: "timeIntervalInput", title: "Run only during a certain time", refresh
 			input "timeStart", "time", title: "Starting", required: false, refreshAfterSelection:true
 			input "timeEnd", "time", title: "Ending", required: false, refreshAfterSelection:true
 		}
-        }
+}
 
 //Version/Copyright/Information/Help
 
@@ -195,16 +195,16 @@ private def textCopyright() {
 
 private def textLicense() {
     def text =
-		"Licensed under the Apache License, Version 2.0 (the 'License'); "+
-		"you may not use this file except in compliance with the License. "+
+		"Licensed under the Apache License, Version 2.0 (the 'License');"+
+		"you may not use this file except in compliance with the License."+
 		"You may obtain a copy of the License at"+
 		"\n\n"+
 		"    http://www.apache.org/licenses/LICENSE-2.0"+
 		"\n\n"+
-		"Unless required by applicable law or agreed to in writing, software "+
-		"distributed under the License is distributed on an 'AS IS' BASIS, "+
-		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. "+
-		"See the License for the specific language governing permissions and "+
+		"Unless required by applicable law or agreed to in writing, software"+
+		"distributed under the License is distributed on an 'AS IS' BASIS,"+
+		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."+
+		"See the License for the specific language governing permissions and"+
 		"limitations under the License."
 }
 
