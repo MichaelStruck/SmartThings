de06b64d52c0799e35e01335a5b775e2a6cc2c56
Michael <MichaelStruck@users.noreply.github.com>
2015-09-13 10:53:12 -0700

    Update and rename Life360 Helper-Phase Change.groovy to Life360 Helper-Routine Change.groovy

--- Other-SmartApps/Life360-Helper/Life360 Helper-Routine Change.groovy
+++ Other-SmartApps/Life360-Helper/Life360 Helper-Routine Change.groovy
@@ -1,10 +1,12 @@
 /**
- *  Life360 Helper-Phrase Change
+ *  Life360 Helper-Routine Change
  *  Version 1.0.0 3/20/15
  *  Version 1.0.1 3/24/15 - Updated with more effecient code
  *  Version 1.0.2 4/9/15 - Added ability to change name of app
  *  Version 1.0.3 5/15/15 - Optimized code, added a switch to limit the trigger to once a day
- *  Version 1.0.4 7/1/15 - Added About screen and updated GUI for more consitency between apps
+ *  Version 1.0.4 7/1/15 - Added About screen and updated GUI for more consistency between apps
+ *  version 1.0.5 7/17/15  - Added ability to not run the app when in certain modes
+ *  Version 1.0.6 9/13/14 - Changed syntax to reflect new SmartThings Routines (from Hello, Home)
  *
  *  Copyright 2015 Michael Struck
  *
@@ -21,10 +23,10 @@
  */
 
 definition(
-    name: "Life360 Helper-Phrase Change",
+    name: "Life360 Helper-Routine Change",
     namespace: "MichaelStruck",
     author: "Michael Struck",
-    description: "Monitors the Life360 presence sensors to trigger differnt Hello, Home phrases based on selected modes.",
+    description: "Monitors the Life360 presence sensors to trigger differnt routines based on selected modes.",
     category: "Convenience",
     iconUrl: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/Life360-Helper/life360.png",
     iconX2Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/Life360-Helper/life360@2x.png",
@@ -34,29 +36,32 @@ preferences {
 	page name: "getPref"
 }
 
-
 def getPref() {
     dynamicPage(name: "getPref", install:true, uninstall: true) {
     
     	section("Choose the Life360 presence sensors you want to monitor...") {
 			input "people", "capability.presenceSensor", multiple: true, title: "Life360 Presence Sensors" 
 		}
-		section("When presence is detected and in one of these modes...") {
-			input "modes", "mode", title: "Mode(s)", required: true, multiple: true
-		}
 		def phrases = location.helloHome?.getPhrases()*.label
 		if (phrases) {
         	phrases.sort()
-			section("Perform the following Hello, Home phrase when...") {
-				input "phrase1", "enum", title: "Any of the modes above are active", required: true, options: phrases
-				input "phrase2", "enum", title: "None of the modes above are active", required: true, options: phrases
-			}
-        }
-        section ("Limit the trigger to once a day") {
-        	input "triggerOnce", title: "Trigger only once per day...", "bool", defaultValue: true
+            
+        	section("When all are present and...") {
+				input "modes1", "mode", title: "In these modes...", required: true, multiple: true
+				input "phrase1", "enum", title: "Perform the following routine", required: true, options: phrases
+        	}
+            
+            section("When all are present and...") {
+				input "modes2", "mode", title: "In these modes...", required: false, multiple: true
+				input "phrase2", "enum", title: "Perform the following routine", required: true, options: phrases
+        	}
+			section ("Do nothing if in one of these modes..."){
+        		input "modesNoAction", "mode", title: "Mode(s)", required: false, multiple: true
+        	}
         }
 		section([mobileOnly:true], "Options") {
-			label(title: "Assign a name", required: false)
+			input "triggerOnce", title: "Trigger only once per day...", "bool", defaultValue: "true"
+            label(title: "Assign a name", required: false)
             href "pageAbout", title: "About ${textAppName()}", description: "Tap to get application version, license and instructions"
 		}  
 	}
@@ -86,20 +91,22 @@ def updated() {
 
 def init(){
 	state.triggered = false
-    def midnightToday = timeToday("2000-01-01T23:59:59.999-0000", location.timeZone)
-    schedule (midnightToday, midNightReset)
-    subscribe(people, "presence", presence)
+    subscribe(people, "presence", presenceHandler)
 }
 
-def presence(evt){
-    if (everyoneIsPresent() && (!triggerOnce || (triggerOnce && !state.triggered))){
-    	if (runMode()) {
-        	location.helloHome.execute(phrase1)
-    	} 
-        else {
-    		location.helloHome.execute(phrase2)
+def presenceHandler(evt){
+	log.debug "Presence detected"
+    if (everyoneIsPresent() && (!triggerOnce || (triggerOnce && !state.triggered)) && !runMode(modesNoAction)){
+    	if (runMode(modes1)) {
+            location.helloHome.execute(phrase1)
     	}
-	state.triggered = true
+        else if (runMode(modes2)) {
+            location.helloHome.execute(phrase2)
+    	}
+    }
+	if (triggerOnce){	
+        state.triggered = true
+    	schedule (getMidnight(), midNightReset)
     }
 }
 
@@ -108,13 +115,18 @@ private everyoneIsPresent() {
     result
 }
 
-private runMode() {
-	def result = modes.contains(location.mode)
+private runMode(modes) {
+    def result = modes ? modes.contains(location.mode) : false
     result
 }
 
 def midNightReset() {
 	state.triggered = false
+}
+
+def getMidnight() {
+	def midnightToday = timeToday("2000-01-01T23:59:59.999-0000", location.timeZone)
+	midnightToday
 }
 
 //Version/Copyright/Information/Help
@@ -124,7 +136,7 @@ private def textAppName() {
 }	
 
 private def textVersion() {
-    def text = "Version 1.0.4 (07/01/2015)"
+    def text = "Version 1.0.6 (09/13/2015)"
 }
 
 private def textCopyright() {
@@ -149,7 +161,7 @@ private def textLicense() {
 private def textHelp() {
 	def text =
     	"In Life360, choose a location you want to monitor. This does NOT need to be the location where the SmartThings " +
-        "hub is located; this could be your work location, or a location on the way home during your commute. Choose this sensor in this app; " +
-        "when you are in a certain mode and this sensor shows 'present' you can activate a Hello, Home phrase. This can be used to pre-heat a house "+ 
-        "or turn on a specific set of lights. "
+        "hub is located; this could be your work location, or a location on the way home during your commute. Choose this sensor; " +
+        "when you are in a certain mode and this sensor shows 'present' you can activate a routine. This can be used to pre-heat a house "+ 
+        "or turn on a specific set of lights. You may also choose a mode in which all actions are bypassed."
 }
