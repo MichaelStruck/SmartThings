bcf01051722b06100dd795a8952537bbc6a62740
Michael <MichaelStruck@users.noreply.github.com>
2015-11-18 05:32:12 -0800

    Create Alexa Helper.groovy

---
+++ Other-SmartApps/AlexaHelper/Alexa Helper.groovy
@@ -0,0 +1,112 @@
+/**
+ *  Alexa Helper-Parent
+ *
+ *  Copyright 2015 Michael Struck
+ *  Version 3.0.0 11/17/15
+ * 
+ *  Version 1.0.0 - Initial release
+ *  Version 2.0.0 - Added 6 slots to allow for one app to control multiple on/off actions
+ *  Version 2.0.1 - Changed syntax to reflect SmartThings Routines (instead of Hello, Home Phrases)
+ *  Version 2.1.0 - Added timers to the first 4 slots to allow for delayed triggering of routines or modes
+ *  Version 2.2.1 - Allow for on/off control of switches and changed the UI slightly to allow for other controls in the future
+ *  Version 2.2.2 - Fixed an issue with slot 4
+ *  Version 3.0.0 - Allow for parent/child 'slots'
+ * 
+ *  Uses code from Lighting Director by Tim Slagle & Michael Struck
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+definition(
+    name: "Alexa Helper",
+    singleInstance: true,
+    namespace: "MichaelStruck",
+    author: "Michael Struck",
+    description: "Allows for routines or modes to be tied to various switch's state controlled by Alexa (Amazon Echo).",
+    category: "My Apps",
+	iconUrl: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa.png",
+    iconX2Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa@2x.png",
+    iconX3Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa@2x.png")
+
+preferences {
+    page(name: "mainPage", title: "Alexa Helper Scenarios", install: true, uninstall: true,submitOnChange: true) {
+            section {
+                    app(name: "childScenarios", appName: "Alexa Helper - Scenarios", namespace: "MichaelStruck", title: "Create New Alexa Scenario...", multiple: true)
+            }
+            section([title:"Options", mobileOnly:true]) {
+            	label title:"Assign a name", required:false
+            	href "pageAbout", title: "About ${textAppName()}", description: "Tap to get application version, license and instructions"
+        	}
+    }
+}
+
+page(name: "pageAbout", title: "About ${textAppName()}") {
+        section {
+            paragraph "${textVersion()}\n${textCopyright()}\n\n${textLicense()}\n"
+        }
+        section("Instructions") {
+            paragraph textHelp()
+        }
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    childApps.each {child ->
+            log.info "Installed Scenario: ${child.label}"
+    }
+}
+
+//Version/Copyright/Information/Help
+
+private def textAppName() {
+	def text = "Alexa Helper"
+}	
+
+private def textVersion() {
+    def text = "Version 3.0.0 (11/17/2015)"
+}
+
+private def textCopyright() {
+    def text = "Copyright © 2015 Michael Struck"
+}
+
+private def textLicense() {
+    def text =
+		"Licensed under the Apache License, Version 2.0 (the 'License'); "+
+		"you may not use this file except in compliance with the License. "+
+		"You may obtain a copy of the License at"+
+		"\n\n"+
+		"    http://www.apache.org/licenses/LICENSE-2.0"+
+		"\n\n"+
+		"Unless required by applicable law or agreed to in writing, software "+
+		"distributed under the License is distributed on an 'AS IS' BASIS, "+
+		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. "+
+		"See the License for the specific language governing permissions and "+
+		"limitations under the License."
+}
+
+private def textHelp() {
+	def text =
+		"Ties SmartThings routines, modes or switches to the on/off state of a specifc switch. "+
+		"Perfect for use with Alexa.\n\nTo use, first create the required momentary button tiles or virtual switches from the SmartThings IDE. "+
+		"You may also use any physical switches already associated with SmartThings. Include these switches within the Echo/SmartThings app, then discover the switches on the Echo. "+
+		"Finally, add a scenario and choose the discovered switch to be monitored and tie the on/off state of that switch to a specific routine, mode or on/off state of other switches. "+
+		"The routine, mode or switches will fire with the switch state change, except in cases where you have a delay specified. This time delay is optional. "+
+        "\n\nPlease note that if you are using a momentary switch you should only define the 'on' action within each scenario." 
+}
