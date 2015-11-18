6199f63a15a5a29c1659e30eb55dec402ec6f4a5
Michael <MichaelStruck@users.noreply.github.com>
2015-11-18 05:35:25 -0800

    Create Alexa Helper-Scenario.groovy

---
+++ Other-SmartApps/AlexaHelper/Alexa Helper-Scenario.groovy
@@ -0,0 +1,225 @@
+/**
+ *  Alexa Helper-Child
+ *
+ *  Copyright 2015 Michael Struck
+ *  Version 1.0.0 11/17/15
+ * 
+ *  Version 1.0.0 - Initial release of child app
+ * 
+ *  Uses code from Lighting Director by Tim Slagle & Michael Struck
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+definition(
+    name: "Alexa Helper-Scenario",
+    namespace: "MichaelStruck",
+    author: "Michael Struck",
+    description: "Child app (do not publish) that allows for routines or modes to be tied to various switch's state controlled by Alexa (Amazon Echo).",
+    category: "Convenience",
+	parent: "MichaelStruck:Alexa Helper",
+    iconUrl: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa.png",
+    iconX2Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa@2x.png",
+    iconX3Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa@2x.png")
+    
+preferences {
+    page name:"pageSetup"
+}
+
+// Show "pageSetup" page
+def pageSetup() {
+	dynamicPage(name: "pageSetup", install: true, uninstall: true) {
+    section("Name your scenario") {
+    		label title:"Scenario Name", required:true
+    	}
+        section("Choose an Alexa switch to use...") {
+			input "AlexaSwitch", "capability.switch", title: "Alexa Switch", multiple: false, required: true
+    		input "momentary", "bool", title: "Is this a momentary switch?", defaultValue: false, submitOnChange:true
+        }
+        def phrases = location.helloHome?.getPhrases()*.label
+		if (phrases) {
+        	phrases.sort()
+		}	
+        section ("When switch is on..."){
+        	if (phrases) {
+            	input "onPhrase", "enum", title: "Perform this routine", options: phrases, required: false
+            }
+        	input "onMode", "mode", title: "Change to this mode", required: false
+            input "onSwitches", "capability.switch", title: "Turn on these switches...", multiple: true, required: false
+        	input "delayOn", "number", title: "Delay in minutes", defaultValue: 0, required: false
+       }
+        
+        if (!momentary) {
+        	section ("When switch is off..."){
+        		if (phrases) {
+            		input "offPhrase", "enum", title: "Perform this routine", options: phrases, required: false
+            	}
+        		input "offMode", "mode", title: "Change to this mode", required: false
+                input "offSwitches", "capability.switch", title: "Turn off these switches...", multiple: true, required: false
+                input "delayOff", "number", title: "Delay in minutes", defaultValue: 0, required: false
+        	}
+        }
+        section("Restrictions") {            
+			input "runDay", "enum", options: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], title: "Only on certain days of the week...",  multiple: true, required: false
+        	href "timeIntervalInput", title: "Only during a certain time...", description: getTimeLabel(timeStart, timeEnd), state: greyedOutTime(timeStart, timeEnd)
+            input "runMode", "mode", title: "Only during the following modes...", multiple: true, required: false
+		}
+    }
+}
+
+page(name: "timeIntervalInput", title: "Only during a certain time") {
+		section {
+			input "timeStart", "time", title: "Starting", required: false
+			input "timeEnd", "time", title: "Ending", required: false
+		}
+} 
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unschedule()
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+	if (AlexaSwitch){
+    	subscribe(AlexaSwitch, "switch", "switchHandler")	
+	}
+}
+
+//Handlers----------------
+def switchHandler(evt) {
+    if ((!runMode || runMode.contains(location.mode)) && getDayOk(runDay) && getTimeOk(timeStart,timeEnd)) {    
+        log.debug "Alexa Helper scenario triggered"
+        if (evt.value == "on" && (onPhrase || onMode || onSwitches)) {
+        	if (!delayOn || delayOn == 0) {
+            	turnOn()
+            }
+            else {
+            	unschedule 
+                runIn(delayOn*60, turnOn, [overwrite: true])
+            }
+    	} 
+    	else if (evt.value == "off" && !momentary && (offPhrase || offMode || offSwitches)) {
+        	if (!delayOff || delayOff == 0) {
+            	turnOff()
+            }
+            else {
+            	runIn(delayOff*60, turnOff, [overwrite: true])
+            }
+    	}
+	}
+}
+
+def turnOn(){
+	if (onPhrase){
+		location.helloHome.execute(onPhrase)
+	}
+	if (onMode) {
+		changeMode(onMode)
+	}
+    if (onSwitches){
+    	onSwitches?.on()
+    }
+}
+
+def turnOff(){
+	if (offPhrase){
+		location.helloHome.execute(offPhrase)
+	}
+	if (offMode) {
+		changeMode(offMode)
+	}
+    if (offSwitches){
+    	offSwitches?.off()
+    }
+}
+
+//Common Methods-------------
+
+def greyOut(scenario){
+    def result = scenario ? "complete" : ""
+}
+
+def getTitle(scenario) {
+	def title = scenario ? scenario : "Empty"
+}
+
+def changeMode(newMode) {
+	if (location.mode != newMode) {
+		if (location.modes?.find{it.name == newMode}) {
+			setLocationMode(newMode)
+		} else {
+			log.debug "Unable to change to undefined mode '${newMode}'"
+		}
+	}
+}
+
+def getTimeLabel(start, end){
+	def timeLabel = "Tap to set"
+	
+    if(start && end){
+    	timeLabel = "Between " + hhmm(start) + " and " +  hhmm(end)
+    }
+    else if (start) {
+		timeLabel = "Start at " + hhmm(start)
+    }
+    else if(end){
+    timeLabel = "End at " + hhmm(end)
+    }
+	timeLabel	
+}
+
+def greyedOutTime(start, end){
+	def result = start || end ? "complete" : ""
+}
+
+private hhmm(time) {
+	new Date().parse("yyyy-MM-dd'T'HH:mm:ss.SSSZ", time).format("h:mm a", timeZone(time))
+}
+
+private getDayOk(dayList) {
+	def result = true
+    if (dayList) {
+		def df = new java.text.SimpleDateFormat("EEEE")
+		if (location.timeZone) {
+			df.setTimeZone(location.timeZone)
+		}
+		else {
+			df.setTimeZone(TimeZone.getTimeZone("America/New_York"))
+		}
+		def day = df.format(new Date())
+		result = dayList.contains(day)
+	}
+    result
+}
+
+private getTimeOk(startTime, endTime) {
+	def result = true
+	if (startTime && endTime) {
+		def currTime = now()
+		def start = timeToday(startTime).time
+		def stop = timeToday(endTime).time
+		result = start < stop ? currTime >= start && currTime <= stop : currTime <= stop || currTime >= start
+	}
+	else if (startTime){
+    	result = currTime >= start
+    }
+    else if (endTime){
+    	result = currTime <= stop
+    }
+    result
+}
