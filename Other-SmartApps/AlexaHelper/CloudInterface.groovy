644904112efb2aecd91a734e7adb2ecfdb888713
Michael <MichaelStruck@users.noreply.github.com>
2015-12-20 19:26:50 -0800

    Update CloudInterface.groovy

--- Other-SmartApps/AlexaHelper/CloudInterface.groovy
+++ Other-SmartApps/AlexaHelper/CloudInterface.groovy
@@ -1,7 +1,7 @@
 /**
  *  Alexa Helper-Cloud Interface
  *
- *	Version 1.0.0 - 12/24/15 Copyright 2015 Michael Struck
+ *  Version 1.0.0 - 12/24/15 Copyright 2015 Michael Struck
  *  
  *  Version 1.0.0 - Initial release
  *
@@ -38,33 +38,33 @@ preferences {
 def mainPage() {
     dynamicPage(name: "mainPage", title:"", install: true, uninstall: true) {
 		section("Allow external control of these devices...") {
-	    	input "switches", "capability.switch", title: "Switches", multiple: true, required: false
-        }
+	    		input "switches", "capability.switch", title: "Switches", multiple: true, required: false
+        	}
         section("URLs"){
-        	href "showURLs", title: "Show URLs", description: "Tap to show URLs to control switches"
-        }
+        		href "showURLs", title: "Show URLs", description: "Tap to show URLs to control switches"
+        	}
         section([title:"Options", mobileOnly:true]) {
-            	label title:"Assign a name", required:false
-            	href "pageAbout", title: "About ${textAppName()}", description: "Tap to get application version, license and instructions"
-        }
+            		label title:"Assign a name", required:false
+            		href "pageAbout", title: "About ${textAppName()}", description: "Tap to get application version, license and instructions"
+        	}
 	}
 }
 
 def showURLs(){
 	dynamicPage(name: "showURLs", title:"Copy the desired URLs to Alexa Helper") {
 		if (!state.accessToken) {
-    		createAccessToken()
+	    		createAccessToken()
 		}
-        def swName=""
-        switches.each{
-        	swName= "${it.label}"
-            section ("Turn on ${swName}") {
+        	def swName=""
+        	switches.each{
+        		swName= "${it.label}"
+        		section ("Turn on ${swName}") {
 				paragraph "", title: "https://graph.api.smartthings.com/api/smartapps/installations/${app.id}/w?l=${swName}&c=on&access_token=${state.accessToken}"
 			}
-            section ("Turn off ${swName}") {
+        		section ("Turn off ${swName}") {
 				paragraph "", title: "https://graph.api.smartthings.com/api/smartapps/installations/${app.id}/w?l=${swName}&c=off&access_token=${state.accessToken}"
-			}
-       }
+			}	
+       		}
 	}
 }
 page(name: "pageAbout", title: "About ${textAppName()}") {
@@ -87,8 +87,8 @@ def updated() {
 }
 
 def initialize() {
-    if (!state.accessToken) {
-    	createAccessToken()
+	if (!state.accessToken) {
+    		createAccessToken()
 	}
 }
 
@@ -98,9 +98,8 @@ mappings {
 
 void writeData() {
 	log.debug "Command received with params $params"
-    
-    def command = params.c  	//The action you want to take i.e. on/off 
-    def label = params.l		//The name given to the device by you
+	def command = params.c  	//The action you want to take i.e. on/off 
+    	def label = params.l		//The name given to the device by you
     
 	if (switches){
 		def device = switches?.find{it.label == label}
@@ -123,7 +122,7 @@ private def textCopyright() {
 }
 
 private def textLicense() {
-    def text =
+	def text =
 		"Licensed under the Apache License, Version 2.0 (the 'License'); "+
 		"you may not use this file except in compliance with the License. "+
 		"You may obtain a copy of the License at"+
@@ -140,6 +139,6 @@ private def textLicense() {
 private def textHelp() {
 	def text =
 		"To be used with Alexa Helper 3.4.0+, this app allows you to define switches (typically virtual) to be controlled via a single url."+
-        "This is useful when using your Alexa with two different SmartThings locations/accounts. Now you can command Alexa at "+
-        "one location and have actions happen at another location also controlled by a SmartThings hub."
+        	"This is useful when using your Alexa with two different SmartThings locations/accounts. Now you can command Alexa at "+
+        	"one location and have actions happen at another location also controlled by a SmartThings hub."
 }
