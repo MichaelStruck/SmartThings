c9570dd16ebd98d19dcbb50d91ca5e912f8150ba
Michael <MichaelStruck@users.noreply.github.com>
2016-01-01 14:50:41 -0800

    Update Nest Helper-Mode Change.groovy

--- Other-SmartApps/Nest-Helper-Mode-Change/Nest Helper-Mode Change.groovy
+++ Other-SmartApps/Nest-Helper-Mode-Change/Nest Helper-Mode Change.groovy
@@ -1,11 +1,12 @@
 /**
  *  Nest Helper-Mode Change
  *
- *  Copyright 2015 Michael Struck
+ *  Copyright 2016 Michael Struck
  *  Version 1.0.0 - 3/26/15
  *  Version 1.0.1 - 4/9/15 Added the ability to change the name of the app and removed modes to run in (redundent)
  *  Version 1.0.2 - 5/31/15 Added About screen and code optimizations
- *  Version 1.0.3 - 6/15/15 Changed internal logic for home and away to allow for better interface layout
+ *  Version 1.0.3 - 8/5/15 Changed internal logic for home and away to allow for better interface layout
+ *  Version 1.0.4 - 1/1/16 Change the about page slightly to align with other apps produced by Michael Struck
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -32,29 +33,32 @@ preferences {
 }
 
 def getPref() {
-    dynamicPage(name: "getPref", install:true, uninstall: true) {
-    	section("Choose a Nest Thermostat...") {
+    dynamicPage(name: "getPref", install:true, uninstall: false) {
+    	section {
     		input "tstat1", "capability.thermostat", title: "Nest Thermostat", multiple: false , required: true
-			}
+		}
 		section("Change Nest HOME/AWAY based on SmartThing modes") {
     		input "awayMode", "mode", required: true, multiple: true, title: "Nest AWAY modes"
 			input "homeMode", "mode", required: false, multiple: true, title: "Nest HOME modes (optional)"
 		}
 		section([mobileOnly:true], "Options") {
 			label(title: "Assign a name", required: false)
-            href "pageAbout", title: "About ${textAppName()}", description: "Tap to get application version, license and instructions"
+            href "pageAbout", title: "About ${textAppName()}", description: "Tap to get application version, license, instructions or remove the application"
 		}
     }
 }
 
-page(name: "pageAbout", title: "About ${textAppName()}") {
-        section {
-            paragraph "${textVersion()}\n${textCopyright()}\n\n${textLicense()}\n"
-        }
-        section("Instructions") {
-            paragraph textHelp()
-        }
+page(name: "pageAbout", title: "About ${textAppName()}", ,uninstall: true) {
+	section {
+    	paragraph "${textVersion()}\n${textCopyright()}\n\n${textLicense()}\n"
+    }
+	section("Instructions") {
+		paragraph textHelp()
+	}
+    section("Tap button below to remove the application"){
+	}
 }
+
 
 //--------------------------------------
 
@@ -93,11 +97,11 @@ private def textAppName() {
 }	
 
 private def textVersion() {
-    def text = "Version 1.0.3 (06/15/2015)"
+    def text = "Version 1.0.4 (01/01/2016)"
 }
 
 private def textCopyright() {
-    def text = "Copyright © 2015 Michael Struck"
+    def text = "Copyright © 2016 Michael Struck"
 }
 
 private def textLicense() {
@@ -119,5 +123,5 @@ private def textHelp() {
 	def text =
     	"Choose a Nest Thermostat that will be linked to various SmartThings modes. " +
         "When the modes change, the Nest Thermostat will change to Home or Away based on the settings. If no HOME settings are specified, "+
-        "then the AWAY mode setting will be the trigger for both events (i.e. In that mode=AWAY, not in that mode=HOME."
+        "then the AWAY mode setting will be the trigger for both events (i.e. In that mode=AWAY, not in that mode=HOME)."
 }
